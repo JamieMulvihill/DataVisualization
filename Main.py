de96be3966cdf3ca19e696dc1e1094ba66106bbd
JamieMulvihill <44208828+JamieMulvihill@users.noreply.github.com>
2025-03-22 12:50:20 +0000

    Added Data, Iris and Wine Reviews

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
-#Main.py
+# Data Visualization
+# Main.py
+
+import pandas as pd
+
+iris = pd.read_csv('iris.csv', names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+print(iris.head())
